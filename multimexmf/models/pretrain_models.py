d69e066252b4dd09cc562b5f016a391ab6ed3a6c
sukhijab <sukhijab@student.ethz.ch>
2024-07-03 12:01:43 -0700

    ensemble model implementation

--- multimexmf/models/pretrain_models.py
+++ multimexmf/models/pretrain_models.py
@@ -93,6 +93,8 @@ class MultiHeadGaussianEnsemble(nn.Module):
         self.min_std = min_std
         self.max_std = max_std
         self.num_heads = num_heads
+        self.input_dim = input_dim
+        self.output_shapes = {k: v.shape[-1] for k, v in output_dict.items()}
         modules = [
             nn.Sequential(
                 nn.Linear(input_dim, features[0]),
@@ -111,11 +113,11 @@ class MultiHeadGaussianEnsemble(nn.Module):
 
         self.feat = nn.ModuleList(modules)
         if self.learn_std:
-            output_modules = [[key, nn.Linear(prev_size, 2 * num_heads * value.shape[-1])]
-                              for key, value in output_dict.items()]
+            output_modules = [[key, nn.Linear(prev_size, 2 * num_heads * shape)]
+                              for key, shape in self.output_shapes.items()]
         else:
-            output_modules = [[key, nn.Linear(prev_size, num_heads * value.shape[-1])]
-                              for key, value in output_dict.items()]
+            output_modules = [[key, nn.Linear(prev_size, num_heads * shape)]
+                              for key, shape in self.output_shapes.items()]
         self.output_modules = nn.ModuleDict(output_modules)
         if disagreement_weights is not None:
             assert disagreement_weights.keys() == output_dict.keys()
@@ -140,10 +142,10 @@ class MultiHeadGaussianEnsemble(nn.Module):
             x = feat(x)
 
         output_dict = {}
-        for key, value in self.output_dict.items():
+        for key, shape in self.output_shapes.items():
             pred = self.output_modules[key](x).reshape(batch_size, -1, self.num_heads)
             if self.learn_std:
-                mean, log_std = torch.split(pred, 2, dim=-2)
+                mean, log_std = torch.split(pred, shape, dim=-2)
                 std = nn.functional.softplus(log_std)
                 std = torch.clamp(std, min=self.min_std, max=self.max_std)
                 output_dict[key] = (mean, std)
@@ -158,11 +160,10 @@ class MultiHeadGaussianEnsemble(nn.Module):
                 assert isinstance(prediction[key], Tuple)
                 # dim: batch, feature, num_ensemble
                 mean, std = prediction[key]
-                var = torch.square(std)
-                loss[key] = (((mean - val[..., None]) ** 2) / var + 2 * torch.log(var)).mean()
+                loss[key] = - (torch.distributions.Normal(mean, std).log_prob(val[..., None]).mean())
             else:
                 mean = prediction[key]
-                loss[key] = ((mean - val[..., None]) ** 2).mean()
+                loss[key] = - (torch.distributions.Normal(mean, torch.ones_like(mean)).log_prob(val[..., None]).mean())
         return loss
 
     def get_disagreement(self, prediction: Dict) -> torch.Tensor:
@@ -176,7 +177,7 @@ class MultiHeadGaussianEnsemble(nn.Module):
                 al_std = std.mean(dim=-1)
                 ratio = torch.square(epistemic_std / al_std)
                 if self.use_entropy:
-                    # take mean over batch dim
+                    # take mean over output dim
                     disagreement[key] = torch.log(1 + ratio).mean(dim=-1) * self.disagreement_weights[key]
                 else:
                     # take mean over batch dim
@@ -185,7 +186,155 @@ class MultiHeadGaussianEnsemble(nn.Module):
                 assert val.shape[-1] == self.num_heads
                 epistemic_var = torch.square(val.std(dim=-1))
                 if self.use_entropy:
+                    # take mean over output dim
+                    disagreement[key] = torch.log(EPS + epistemic_var).mean(dim=-1) * self.disagreement_weights[key]
+                else:
                     # take mean over batch dim
+                    disagreement[key] = epistemic_var.mean(dim=-1) * self.disagreement_weights[key]
+
+        total_disagreements = torch.stack([val.reshape(-1, 1) for val in disagreement.values()], dim=-1)
+        if self.agg_disag == 'max':
+            return total_disagreements.max(dim=-1)
+        elif self.agg_disag == 'sum':
+            return total_disagreements.sum(dim=-1)
+        else:
+            raise NotImplementedError
+
+
+class SimpleMLP(nn.Module):
+    def __init__(self,
+                 input_dim: int,
+                 output_dim: Optional[int] = None,
+                 features: Tuple = (256, 256, 256),
+                 act_fn: nn.Module = nn.ReLU(),
+                 learn_std: bool = False,
+                 ):
+        super().__init__()
+        self.learn_std = learn_std
+        modules = [
+            nn.Sequential(
+                nn.Linear(input_dim, features[0]),
+                act_fn,
+            )
+        ]
+        prev_size = features[0]
+        for i in range(1, len(features)):
+            modules.append(
+                nn.Sequential(
+                    nn.Linear(prev_size, features[i]),
+                    act_fn,
+                )
+            )
+            prev_size = features[i]
+        self.output_dim = output_dim
+        if output_dim:
+            if self.learn_std:
+                modules.append(nn.Linear(prev_size, 2 * output_dim))
+            else:
+                modules.append(nn.Linear(prev_size, output_dim))
+
+        self.feat = nn.ModuleList(modules)
+
+    def forward(self, x) -> Dict:
+        for feat in self.feat:
+            x = feat(x)
+        return x
+
+
+class EnsembleMLP(nn.Module):
+    def __init__(self, input_dim: int, output_dict: Dict, num_heads: int = 5,
+                 features: Tuple = (256, 256, 256),
+                 act_fn: nn.Module = nn.ReLU(), learn_std: bool = False, min_std: float = 1e-3, max_std: float = 1e2,
+                 disagreement_weights: Optional[Dict] = None, use_entropy: bool = True, agg_disag: str = 'sum',
+                 optimizer_class: Type[torch.optim.Optimizer] = torch.optim.Adam,
+                 optimizer_kwargs: Optional[Dict[str, Any]] = None,
+                 ):
+        super().__init__()
+        self.learn_std = learn_std
+        self.num_heads = num_heads
+        self.output_shapes = {k: v.shape[-1] for k, v in output_dict.items()}
+        self.min_std = min_std
+        self.max_std = max_std
+        output_dim = sum(self.output_shapes.values())
+        self.models = nn.ModuleList([SimpleMLP(
+            input_dim=input_dim,
+            features=features,
+            output_dim=output_dim,
+            act_fn=act_fn,
+            learn_std=learn_std,
+        ) for _ in range(num_heads)])
+
+        if disagreement_weights is not None:
+            assert disagreement_weights.keys() == output_dict.keys()
+            self.disagreement_weights = disagreement_weights
+        else:
+            self.disagreement_weights = {k: 1.0 for k in output_dict.keys()}
+
+        self.use_entropy = use_entropy
+        self.agg_disag = agg_disag
+        self.output_dict = output_dict
+
+        if optimizer_kwargs is None:
+            optimizer_kwargs = {}
+            # Small values to avoid NaN in Adam optimizer
+            if optimizer_class == torch.optim.Adam:
+                optimizer_kwargs["eps"] = 1e-5
+
+        self.optimizer = optimizer_class(self.parameters(), **optimizer_kwargs)
+
+    def forward(self, x) -> Dict:
+        # concatenate outputs of the ensemble
+        outputs = torch.cat([model(x)[..., None] for model in self.models], dim=-1)
+        if self.learn_std:
+            # split by 2 x output size if learning stds
+            outputs = torch.split(outputs, [2 * val for val in self.output_shapes.values()], dim=-2)
+        else:
+            outputs = torch.split(outputs, [val for val in self.output_shapes.values()], dim=-2)
+        output_dict = {}
+        for i, (key, val) in enumerate(self.output_shapes.items()):
+            if self.learn_std:
+                mean, log_std = torch.split(outputs[i], val, dim=-2)
+                std = nn.functional.softplus(log_std)
+                std = torch.clamp(std, min=self.min_std, max=self.max_std)
+                output_dict[key] = (mean, std)
+            else:
+                output_dict[key] = outputs[i]
+        return output_dict
+
+    def loss(self, prediction: Dict, target: Dict):
+        loss = {}
+        for key, val in target.items():
+            if self.learn_std:
+                assert isinstance(prediction[key], Tuple)
+                # dim: batch, feature, num_ensemble
+                mean, std = prediction[key]
+                loss[key] = - (torch.distributions.Normal(mean, std).log_prob(val[..., None]).mean())
+            else:
+                mean = prediction[key]
+                loss[key] = - (torch.distributions.Normal(mean, torch.ones_like(mean)).log_prob(val[..., None]).mean())
+        return loss
+
+    def get_disagreement(self, prediction: Dict) -> torch.Tensor:
+        disagreement = {}
+        for key, val in prediction.items():
+            if self.learn_std:
+                assert isinstance(prediction[key], Tuple)
+                mean, std = val
+                assert mean.shape[-1] == self.num_heads and std.shape[-1] == self.num_heads
+                epistemic_std = mean.std(dim=-1)
+                al_std = std.mean(dim=-1)
+                ratio = torch.square(epistemic_std / al_std)
+                if self.use_entropy:
+                    # take mean over output dim
+                    disagreement[key] = torch.log(1 + ratio).mean(dim=-1) * self.disagreement_weights[key]
+                else:
+                    # take mean over batch dim
+                    disagreement[key] = ratio.mean(-1) * self.disagreement_weights[key]
+            else:
+                assert val.shape[-1] == self.num_heads
+                epistemic_var = torch.square(val.std(dim=-1))
+                if self.use_entropy:
+                    # take mean over output dim
                     disagreement[key] = torch.log(EPS + epistemic_var).mean(dim=-1) * self.disagreement_weights[key]
                 else:
                     # take mean over batch dim
@@ -201,7 +350,7 @@ class MultiHeadGaussianEnsemble(nn.Module):
 
 
 if __name__ == '__main__':
-    learn_std = True
+    learn_std = False
     from torch.utils.data import TensorDataset, DataLoader
     import matplotlib.pyplot as plt
 
@@ -215,10 +364,10 @@ if __name__ == '__main__':
     ys = ys + noise_level * torch.randn(size=ys.shape)
     train_loader = DataLoader(TensorDataset(xs, ys), shuffle=True, batch_size=32)
     model = MultiHeadGaussianEnsemble(input_dim=1, output_dict={'y': ys}, features=(256, 256),
-                                      optimizer_kwargs={'lr': 1e-3, 'weight_decay': 1e-4}, num_heads=10,
+                                      optimizer_kwargs={'lr': 1e-3, 'weight_decay': 1e-4}, num_heads=5,
                                       learn_std=learn_std)
 
-    n_epochs = 10_000
+    n_epochs = 1000
     n_steps = 0
     for i in range(n_epochs):
         for X_batch, Y_batch in train_loader:
@@ -245,7 +394,7 @@ if __name__ == '__main__':
     test_xs = test_xs.cpu().numpy()
     test_ys = test_ys.cpu().numpy()
     y_pred = y_pred.cpu().detach().numpy()
-
+    scale = 2
     for j in range(output_dim):
         plt.scatter(xs.reshape(-1), ys[:, j], label='Data', color='red')
         for i in range(model.num_heads):
@@ -254,8 +403,8 @@ if __name__ == '__main__':
         eps_std = y_pred.std(axis=-1)
         plt.plot(test_xs, y_mean[..., j], label='Mean', color='blue')
         plt.fill_between(test_xs.reshape(-1),
-                         (y_mean[..., j] - 2 * eps_std[..., j]).reshape(-1),
-                         (y_mean[..., j] + 2 * eps_std[..., j]).reshape(-1),
+                         (y_mean[..., j] - scale * eps_std[..., j]).reshape(-1),
+                         (y_mean[..., j] + scale * eps_std[..., j]).reshape(-1),
                          label=r'$2\sigma$', alpha=0.3, color='blue')
         handles, labels = plt.gca().get_legend_handles_labels()
         plt.plot(test_xs.reshape(-1), test_ys[:, j], label='True', color='green')
